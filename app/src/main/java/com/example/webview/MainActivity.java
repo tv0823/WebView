12ad5873aae66da3cdcf052c4d45bdc7865d4de5
tv0823 <tv0823@bs.amalnet.k12.il>
2024-02-07 12:44:36 +0200

    working web browser

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText eT;
+    WebView wV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eT = findViewById(R.id.eT);
+        wV = findViewById(R.id.wV);
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.setWebViewClient(new MyWebViewClient());
     }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public void pressed(View view) {
+        String url = eT.getText().toString();
+        wV.loadUrl(url);
+    }
+
 }
