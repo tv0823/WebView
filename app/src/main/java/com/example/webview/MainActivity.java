4fe64600972709c756b1f234f584ffc91718c604
tv0823 <tv0823@bs.amalnet.k12.il>
2024-02-17 19:13:05 +0200

    remove import button

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
